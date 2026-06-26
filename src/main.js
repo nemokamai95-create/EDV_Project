24ff9b2ad354852dd1789421138bde9407184b43
42ovsi1bav <42ovsi1bav@hft-stuttgart.de>
2026-06-26 21:28:59 +0200

    adasdawcawf

--- src/main.js
+++ src/main.js
@@ -28,9 +28,30 @@ const viewer = new Viewer("cesiumContainer", {
 async function load() {
   const tileset = await Cesium3DTileset.fromIonAssetId(4957605);
 
+  console.log("asdasa")
   viewer.scene.primitives.add(tileset);
 
   await tileset.readyPromise;
+    const boundingSphere = tileset.boundingSphere;
+    const cartographic = Cartographic.fromCartesian(boundingSphere.center);
+
+    const lon = CesiumMath.toDegrees(cartographic.longitude);
+    const lat = CesiumMath.toDegrees(cartographic.latitude);
+    console.log(lon, lat);
+
+
+
+    alert("Script loaded");
+    const d = 0.005; // roughly 500 m
+
+    const south = lat - d;
+    const north = lat + d;
+    const west = lon - d;
+    const east = lon + d;
+    
+
+    loadRoads(south, west, north, east);
+
 
     viewer.flyTo(tileset, {
     duration: 3,
@@ -67,5 +88,42 @@ handler.setInputAction((movement) => {
     feature.color = Color.YELLOW;
   }
 }, ScreenSpaceEventType.LEFT_CLICK);
+
+
+async function loadRoads(south, west, north, east) {
+
+    const query = `
+    [out:json][timeout:25];
+    (
+      way["highway"](${south},${west},${north},${east});
+    );
+    out geom;
+    `;
+
+    const response = await fetch("https://overpass-api.de/api/interpreter", {
+        method: "POST",
+        body: query
+    });
+
+    const data = await response.json();
+
+    const roads = data.elements.filter(e => e.type === "way");
+
+    roads.forEach(road => {
+        if (!road.geometry) return;
+
+        const positions = road.geometry.map(p =>
+            Cartesian3.fromDegrees(p.lon, p.lat)
+        );
+
+        viewer.entities.add({
+            polyline: {
+                positions,
+                width: 2,
+                material: Color.YELLOW
+            }
+        });
+    });
+}
 
 
